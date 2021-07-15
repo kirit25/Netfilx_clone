29baac14df865ba094114b6493abe46f75fddfd0
kirit25parmar@gmail.com <kirit25parmar@gmail.com>
2021-07-15 11:19:10 +0530

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const Card = ({ movie }) =>{
+    const [isShown, setIsShown] = useState(false)
+    
+    return(
+        <div className="card"
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}
+          >
+            {!isShown && (<video className="video" controls>
+                <source src={movie.thumbnail} type="video/mp4"/>
+            </video>)}
+            
+            {isShown && (
+            <>    
+                <video className="video" controls autoPlay={true} loop>
+                    <source src={movie.thumbnail} type="video/mp4"/>
+            </video>
+            <div className="info-box">
+                <h4>{movie.title}</h4>
+                <p>{movie.synopsis}</p>
+            </div>
+            </>
+        )}
+        </div>
+    )
+}
+export default Card
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const NavBar =() => {
+    return(
+        <div className="navbar">
+            <ul>
+                <li>
+                    <div className="logo"></div>
+                </li>
+                <li><a href="/">Home</a></li>
+            </ul>
+        </div>
+    )
+}
+export default NavBar
